b2156907b0043f7708d93acd9ad3b16d3a1a581c
lhy <406608580@qq.com>
2019-10-27 17:06:05 +0800

    1

--- src/main/java/test/MyServlet.java
+++ src/main/java/test/MyServlet.java
@@ -12,9 +12,9 @@ public class MyServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
     }
-    
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+        request.getRequestDispatcher("/hello1.jsp").forward(request,response);
     }
 }
